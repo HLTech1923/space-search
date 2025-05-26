bbe0e3cf13844897223e4d6469c43a85377ba9f2
KP <kp@gmail.com>
2025-05-26 13:03:08 +0700

    Remove discount kafka

--- src/kafka/consumer.ts
+++ src/kafka/consumer.ts
@@ -59,11 +59,3 @@ export const startKafkaConsumer = async () => {
   });
 };
 
-export const shutdownKafkaConsumer = async () => {
-  try {
-    await consumer.disconnect();
-    console.log("Kafka consumer disconnected");
-  } catch (error) {
-    console.error("Failed to disconnect Kafka consumer", error);
-  }
-};
